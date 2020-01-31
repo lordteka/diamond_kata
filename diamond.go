51388948f032211655092d9f085f96360cd63441
lordteka <lordteka@users.noreply.github.com>
2020-02-01 00:57:01 +0100

    diamond tests

---
+++ diamond.go
@@ -0,0 +1,42 @@
+package main
+
+/*import {
+	"fmt"
+}*/
+
+var Diamond [26]string = [26]string {
+	"                         A",
+	"                        B B",
+	"                       C   C",
+	"                      D     D",
+	"                     E       E",
+	"                    F         F",
+	"                   G           G",
+	"                  H             H",
+	"                 I               I",
+	"                J                 J",
+	"               K                   K",
+	"              L                     L",
+	"             M                       M",
+	"            N                         N",
+	"           O                           O",
+	"          P                             P",
+	"         Q                               Q",
+	"        R                                 R",
+	"       S                                   S",
+	"      T                                     T",
+	"     U                                       U",
+	"    V                                         V",
+	"   W                                           W",
+	"  X                                             X",
+	" Y                                               Y",
+	"Z                                                 Z",
+}
+
+func makeDiamond(letter byte) string {
+	return "toto"
+}
+
+func main() {
+
+}
